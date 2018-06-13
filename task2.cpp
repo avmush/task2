6c7062229daebe5c1ee5ec14f5fec9d8041e325b
avmush <38854789+avmush@users.noreply.github.com>
2018-06-13 14:23:42 +0400

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+ int main () {
+  int x,y;
+  cout<<"enter x"<<endl;
+  cin>>x;
+  cout<<"enter y"<<endl;
+  cin>>y;
+  int ** arr = new int * [x];
+   for(int m = 0; m < x; ++m){
+     arr [m] = new int [y];    
+  } 
+   for(int i = 0; i < x; ++i){
+       for(int j = 0; j < y; ++j){
+           arr[i][j] = rand()%50;}
+   }
+   for(int i = 0; i < x; ++i){
+       for(int j = 0; j < y; ++j){
+         cout<<arr[i][j]<<" / ";}
+       cout<<endl;
+}
+   for(int ix = 0; ix < x; ++ix){
+     delete [] arr[ix];}
+   delete arr;
+
+}
+
